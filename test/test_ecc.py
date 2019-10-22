9b67dc20092b0151e60ca073f89db40fb01734ef
rlee287 <rlee287@yahoo.com>
2019-10-22 17:32:02 -0400

    Decrease repetitions for hamming property tests

--- test/test_ecc.py
+++ test/test_ecc.py
@@ -3,17 +3,17 @@ import random
 from voicechat_modem_dsp.encoders.ecc.hamming_7_4 import *
 
 def test_property_corrupt_hamming_nonmangle():
-    for _ in range(256):
+    for _ in range(64):
         n=random.randint(1,256)
-        data_test=bytearray(random.getrandbits(8) for _ in range(n))
+        data_test=bytearray((random.getrandbits(8) for _ in range(n)))
         hamming_data_test=bytearray(hamming_encode_7_4(data_test))
         recovered_perfect=hamming_decode_7_4(hamming_data_test)
         assert data_test==recovered_perfect
 
 def test_property_corrupt_hamming_recoverable_err():
-    for _ in range(256):
+    for _ in range(64):
         n=random.randint(1,256)
-        data_test=bytearray(random.getrandbits(8) for _ in range(n))
+        data_test=bytearray((random.getrandbits(8) for _ in range(n)))
         hamming_data_test=bytearray(hamming_encode_7_4(data_test))
         for index in range(0,8*n,7):
             index_offset=index+random.randrange(7)

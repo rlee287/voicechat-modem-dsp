29904c98e9bdbf247bf802ab4db4e75053129243
rlee287 <rlee287@yahoo.com>
2019-10-23 14:38:44 -0400

    Continue working on encoder functions and tests

---
+++ test/test_encoders.py
@@ -0,0 +1,94 @@
+import random
+
+import pytest
+
+from voicechat_modem_dsp.encoders.encode_pad import *
+
+def test_unit_bad_datastream_len():
+    datastream_singular=[0]
+    with pytest.raises(ValueError,match=r".*length"):
+        base_2_decode(datastream_singular)
+    with pytest.raises(ValueError,match=r".*length"):
+        base_4_decode(datastream_singular)
+    with pytest.raises(ValueError,match=r".*length"):
+        base_16_decode(datastream_singular)
+    # Base256 cannot have bad length
+
+def test_unit_bad_datastream():
+    datastream_bad_align=[0,1,2,3,-4,5,6,100]
+    with pytest.raises(ValueError,match=r"Illegal symbol.*"):
+        base_2_decode(datastream_bad_align)
+    with pytest.raises(ValueError,match=r"Illegal symbol.*"):
+        base_4_decode(datastream_bad_align)
+    with pytest.raises(ValueError,match=r"Illegal symbol.*"):
+        base_16_decode(datastream_bad_align)
+    with pytest.raises(ValueError,match=r"Illegal symbol.*"):
+        base_256_decode(datastream_bad_align)
+
+def test_unit_base_2():
+    bitstream=b"\x0f"
+    bitstream_list=[0,0,0,0,1,1,1,1]
+    encode_list=base_2_encode(bitstream)
+    assert encode_list==bitstream_list
+    bitstream_list.reverse()
+    decode_reverse=base_2_decode(bitstream_list)
+    assert decode_reverse==b"\xf0"
+
+def test_property_base_2_turnaround():
+    for _ in range(64):
+        n=random.randint(1,256)
+        data_bitstream=bytearray((random.getrandbits(8) for _ in range(n)))
+        data_datastream=base_2_encode(data_bitstream)
+        data_bitstream_recover=base_2_decode(data_datastream)
+        assert bytes(data_bitstream)==data_bitstream_recover
+
+def test_unit_base_4():
+    bitstream=b"\x0f\x81"
+    bitstream_list=[0,0,3,3,2,0,0,1]
+    encode_list=base_4_encode(bitstream)
+    assert encode_list==bitstream_list
+    bitstream_list.reverse()
+    decode_reverse=base_4_decode(bitstream_list)
+    assert decode_reverse==b"\x42\xf0"
+
+def test_property_base_4_turnaround():
+    for _ in range(64):
+        n=random.randint(1,256)
+        data_bitstream=bytearray((random.getrandbits(8) for _ in range(n)))
+        data_datastream=base_4_encode(data_bitstream)
+        data_bitstream_recover=base_4_decode(data_datastream)
+        assert bytes(data_bitstream)==data_bitstream_recover
+
+def test_unit_base_16():
+    bitstream=b"\x01\x23\x45\x67\x89\xab\xcd\xef"
+    bitstream_list=list(range(16))
+    encode_list=base_16_encode(bitstream)
+    assert encode_list==bitstream_list
+    bitstream_list.reverse()
+    decode_reverse=base_16_decode(bitstream_list)
+    assert decode_reverse==b"\xfe\xdc\xba\x98\x76\x54\x32\x10"
+
+def test_property_base_16_turnaround():
+    for _ in range(64):
+        n=random.randint(1,256)
+        data_bitstream=bytearray((random.getrandbits(8) for _ in range(n)))
+        data_datastream=base_16_encode(data_bitstream)
+        data_bitstream_recover=base_16_decode(data_datastream)
+        assert bytes(data_bitstream)==data_bitstream_recover
+
+def test_unit_base_256():
+    bitstream=b"\x01\x23\x45\x67\x89\xab\xcd\xef"
+    bitstream_list=[0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef]
+    encode_list=base_256_encode(bitstream)
+    assert encode_list==bitstream_list
+    bitstream_list.reverse()
+    decode_reverse=base_256_decode(bitstream_list)
+    assert decode_reverse==b"\xef\xcd\xab\x89\x67\x45\x23\x01"
+
+def test_property_base_256_turnaround():
+    for _ in range(64):
+        n=random.randint(1,256)
+        data_bitstream=bytearray((random.getrandbits(8) for _ in range(n)))
+        data_datastream=base_256_encode(data_bitstream)
+        data_bitstream_recover=base_256_decode(data_datastream)
+        assert bytes(data_bitstream)==data_bitstream_recover
--- voicechat_modem_dsp/encoders/encode_pad.py
+++ voicechat_modem_dsp/encoders/encode_pad.py
@@ -1,14 +1,25 @@
-from .bitstream import read_bitstream_iterator
+from .bitstream import read_bitstream_iterator, write_bitstream
 
 import base64
 
-# In custom encoders, return bytes as output type
-# for compatibility with base64 module functions
+# These return a list of numbers corresponding to symbols
 def base_2_encode(bitstream):
     return_list=list()
     for bit in read_bitstream_iterator(bitstream):
         return_list.append(1 if bit else 0)
-    return bytes("".join((str(x) for x in return_list)))
+    return return_list
+
+def base_2_decode(datastream):
+    base_2_mapping={0:False, 1:True}
+    if len(datastream)%8 != 0:
+        raise ValueError("Inappropriate datastream length")
+    return_bytearray=bytearray(len(datastream)//8)
+    for index,bit in enumerate(datastream):
+        try:
+            write_bitstream(return_bytearray,index,base_2_mapping[bit])
+        except KeyError:
+            raise ValueError("Illegal symbol detected in datastream")
+    return bytes(return_bytearray)
 
 def base_4_encode(bitstream):
     return_list=list()
@@ -22,7 +33,24 @@ def base_4_encode(bitstream):
         else:
             rollover_counter+=(2 if bit else 0)
         emit_symbol=not emit_symbol
-    return bytes("".join((str(x) for x in return_list)))
+    return return_list
+
+def base_4_decode(datastream):
+    base_4_mapping={0:(False,False),
+                    1:(False,True),
+                    2:(True,False),
+                    3:(True,True)}
+    if len(datastream)%4 != 0:
+        raise ValueError("Inappropriate datastream length")
+    return_bytearray=bytearray(len(datastream)//4)
+    for index,symbol in enumerate(datastream):
+        try:
+            symbol_entry=base_4_mapping[symbol]
+            write_bitstream(return_bytearray,2*index,symbol_entry[0])
+            write_bitstream(return_bytearray,2*index+1,symbol_entry[1])
+        except KeyError:
+            raise ValueError("Illegal symbol detected in datastream")
+    return bytes(return_bytearray)
 
 def base_8_encode(bitstream):
     return_list=list()
@@ -43,12 +71,24 @@ def base_8_encode(bitstream):
             rollover_counter+=1
     if emit_symbol_counter!=0:
         return_list.append(rollover_counter)
-    return bytes("".join((str(x) for x in return_list)))
+    return return_list
 
 def base_16_encode(bitstream):
-    return base64.b16encode(bitstream)
+    return [int(chr(c), 16) for c in base64.b16encode(bitstream)]
+
+def base_16_decode(datastream):
+    if len(datastream)%2!=0:
+        raise ValueError("Inappropriate datastream length")
+    for c in datastream:
+        # Should only require a single hex digit
+        if c<0 or hex(c)[-2]!="x":
+            raise ValueError("Illegal symbol detected in datastream")
+    return base64.b16decode(bytearray("".join([hex(c)[-1] for c in datastream]),
+                            "ascii"),casefold=True)
 
 def base_32_encode(bitstream):
+    base_32_mapping = {char:int_val for int_val,char in 
+        enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567")}
     bytearr_ret=bytearray(base64.b32encode(bitstream))
     # Remove padding because we do not need it for this use case
     bytearr_ret.replace("=","")
@@ -60,10 +100,18 @@ def base_64_encode(bitstream):
     bytearr_ret.replace("=","")
     return bytes(bytearr_ret)
 
-# Bitstream already base 256 so this is a no-op
+# Bitstream already base 256 so conversion is a no-op
 def base_256_encode(bitstream):
-    return bitstream
+    return list(bitstream)
 
+def base_256_decode(datastream):
+    # Catch ValueError to provide our own message here
+    try:
+        return bytes(datastream)
+    except ValueError:
+        raise ValueError("Illegal symbol detected in datastream")
+
+# Deliberately use NaN here for NumPy propagation later
 def make_pad_array(datastream, pad_len):
     list_ret=[float("nan")]*pad_len
     list_ret+=list(datastream)

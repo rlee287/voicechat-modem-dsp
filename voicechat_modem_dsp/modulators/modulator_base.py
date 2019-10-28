432d8999b28232b447f99c81446beb3ede10514f
rlee287 <rlee287@yahoo.com>
2019-10-28 13:49:06 -0400

    Move samples_per_symbol helper into base modulator class

--- voicechat_modem_dsp/modulators/modulator_base.py
+++ voicechat_modem_dsp/modulators/modulator_base.py
@@ -4,11 +4,16 @@ from abc import ABC, abstractmethod
 
 """
 Base class for other modulator objects
+Also contains useful helper functions as static methods
 """
 class Modulator(ABC):
     @staticmethod
     def generate_timearray(dt, sample_count):
         return np.arange(0,dt*sample_count,dt)
+    @staticmethod
+    def samples_per_symbol(dt, baud):
+        return (1/baud)/dt
+
     @abstractmethod
     def modulate(self, data):
         raise NotImplementedError

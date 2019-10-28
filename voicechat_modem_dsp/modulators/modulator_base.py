ff91ce638bd8dd534ed368eac85d244cb0a07409
rlee287 <rlee287@yahoo.com>
2019-10-28 12:38:30 -0400

    Make modulator a proper abstract class

--- voicechat_modem_dsp/modulators/modulator_base.py
+++ voicechat_modem_dsp/modulators/modulator_base.py
@@ -1,13 +1,17 @@
 import numpy as np
 
+from abc import ABC, abstractmethod
+
 """
 Base class for other modulator objects
 """
-class Modulator(object):
+class Modulator(ABC):
     @staticmethod
     def generate_timearray(dt, sample_count):
         return np.arange(0,dt*sample_count,dt)
+    @abstractmethod
     def modulate(self, data):
         raise NotImplementedError
+    @abstractmethod
     def demodulate(self, time_array, datastream):
         raise NotImplementedError

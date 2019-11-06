9ed0b1d3eb4150f1207d83cbcc36a366e1979961
rlee287 <rlee287@yahoo.com>
2019-11-05 19:57:46 -0500

    Remove WROG annotation from FSK

--- voicechat_modem_dsp/modulators/modulator_fsk.py
+++ voicechat_modem_dsp/modulators/modulator_fsk.py
@@ -5,7 +5,7 @@ class FSKModulator(Modulator):
         if baud>=0.5*max(freq_list):
             raise ValueError("Baud is too high to be modulated "+
                              "using given frequencies")
-        if fs<=2*max(freq_list): #WROG?
+        if fs<=2*max(freq_list):
             raise ValueError
         self.fs=fs
         self.freq_list=freq_list

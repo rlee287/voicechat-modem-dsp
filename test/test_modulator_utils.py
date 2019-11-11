fcb8a867e8f2685a9288f2615cb0b2ab715ddef5
rlee287 <rlee287@yahoo.com>
2019-11-11 15:54:23 -0500

    Write more tests for the average function

--- test/test_modulator_utils.py
+++ test/test_modulator_utils.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
 
+import pytest
+
 from voicechat_modem_dsp.modulators.modulator_utils import *
 
 epsilon=1e-14
@@ -13,7 +15,7 @@ def test_property_generate_timearray():
         assert all(np.abs(np.diff(timearr)-1/n) < epsilon)
         assert np.abs(timearr[-1]-1) < epsilon
 
-def test_unit_average():
+def test_unit_average_int():
     dataseq=np.asarray([1,1,4,1])
     assert average_interval_data(dataseq,0,3)==2
 
@@ -21,5 +23,16 @@ def test_unit_average():
     assert average_interval_data(dataseq,1,2)==2.5
     assert average_interval_data(dataseq,2,3)==2.5
 
-def test_property_average_uniform():
+def test_unit_average_float():
+    dataseq=np.asarray([2,2,2,3,4])
+    assert average_interval_data(dataseq,0.5,1.75)==2
+
+def test_unit_average_invalid():
+    dataseq=[4,1,3,6,1,2,10,2,5]
+    with pytest.raises(ValueError, match=r".*must be larger than.*"):
+        average_interval_data(dataseq,4.9,1.6)
+    with pytest.raises(ValueError, match=r"Invalid index.*"):
+        average_interval_data(dataseq,-1,28.9)
+
+def test_property_average_triangle():
     pass

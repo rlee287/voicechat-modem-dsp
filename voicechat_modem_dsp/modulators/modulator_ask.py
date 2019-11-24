9fd1c2cab766a31e0164c8f7e25fedac4093f2c0
rlee287 <rlee287@yahoo.com>
2019-11-24 16:13:26 -0500

    Create basic ASK modulator code
    Fine-tune parameters later

--- voicechat_modem_dsp/modulators/modulator_ask.py
+++ voicechat_modem_dsp/modulators/modulator_ask.py
@@ -15,18 +15,28 @@ class ASKModulator(Modulator):
             raise ValueError("Invalid amplitudes given")
         self.fs=fs
         self.amp_list=dict(enumerate(amp_list))
+        self.carrier_freq=carrier
         self.baud=baud
     
     def modulate(self, data):
         samples_symbol=modulator_utils.samples_per_symbol(self.fs, self.baud)
+        #sigma_t = w/4k, explain later
+        gaussian_sigma=(1/self.baud)/(4*2.5)
+        print(gaussian_sigma)
+        gaussian_window=modulator_utils.compute_gaussian_window(self.fs,gaussian_sigma)
 
-        amplitude_data=[0]+[amp_list[datum] for datum in data]+[0]
-        time_array=Modulator.generate_time_array(
-            self.fs,len(amplitude_data)*samples_symbol)
+        amplitude_data=[0]+[self.amp_list[datum] for datum in data]+[0]
+        interp_sample_count=np.ceil(len(amplitude_data)*samples_symbol)
+        time_array=modulator_utils.generate_timearray(
+            self.fs,interp_sample_count)
+
         interpolated_amplitude=modulator_utils.previous_resample_interpolate(
             time_array, self.baud, amplitude_data)
-        
+        shaped_amplitude=np.convolve(interpolated_amplitude,gaussian_window,
+            "same")
 
+        return (time_array,shaped_amplitude * \
+            np.sin(2*np.pi*self.carrier_freq*time_array))
 
     def demodulate(self, time_array, datastream):
         raise NotImplementedError

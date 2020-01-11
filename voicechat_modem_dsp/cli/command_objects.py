5dc9c3e7beae88ca88bd4f0f74f4963f653c41f6
rlee287 <rlee287@yahoo.com>
2020-01-11 09:56:42 -0800

    Use cleaner error handling by raising CLIError and catching in decorator

--- voicechat_modem_dsp/cli/command_objects.py
+++ voicechat_modem_dsp/cli/command_objects.py
@@ -1,6 +1,20 @@
 import cleo
 
+import functools
 import os
+
+class CLIError(Exception):
+    pass
+
+def exit_on_error(func):
+    def catch_error_and_exit(self,*args,**kwargs):
+        try:
+            return func(self,*args,**kwargs)
+        except CLIError as e:
+            self.line_error(*e.args)
+            return 1
+    functools.update_wrapper(catch_error_and_exit,func)
+    return catch_error_and_exit
 
 class TxFile(cleo.Command):
     """
@@ -13,29 +27,32 @@ class TxFile(cleo.Command):
         {--no-preamble : Do not include audio preamble with 
             modulation information}
         {--no-toneburst : Do not include calibration toneburst}
+        {--raw : Shortcut for --no-preamble --no-toneburst}
     """
 
+    @exit_on_error
     def handle(self):
         config_file_name=self.option("config")
         output_file_name=self.option("output")
-        self.line("Test base command")
-        if not config_file_name or not os.path.isfile(config_file_name):
-            self.line_error("A valid configuration file must be specified.",
-                "error")
-            return 1
+
+        # Check validity of command line options
+        if not config_file_name:
+            raise CLIError("A configuration file must be specified.","error")
+        if not os.path.isfile(config_file_name):
+            raise CLIError("Config file specified does not exist.","error")
 
         if os.path.exists(output_file_name):
             if os.path.isdir(output_file_name):
-                self.line_error("Output file {} must be writable as a file."
+                raise CLIError("Output file {} must be writable as a file."
                     .format(output_file_name),"error")
-                return 1
 
             if self._io.is_interactive():
                 result=self.confirm("Output file {} already exists. Overwrite?"
                     .format(output_file_name))
                 self.line(str(result))
+                if not result:
+                    return 0
             else:
-                self.line_error("Output file {} already exists "
+                raise CLIError("Output file {} already exists "
                     "and program is in noninteractive mode."
                     .format(output_file_name),"error")
-                return 1

7da9855bae3fbdb36ab8268db4a3e769fc633fac
rlee287 <rlee287@yahoo.com>
2020-01-11 22:00:07 -0800

    Correct types for __all__ in modulators

--- voicechat_modem_dsp/modulators/__init__.py
+++ voicechat_modem_dsp/modulators/__init__.py
@@ -2,4 +2,6 @@ from .modulator_ask import ASKModulator
 from .modulator_fsk import FSKModulator
 from .modulator_utils import ModulationIntegrityWarning
 
-__all__=[ASKModulator, FSKModulator, ModulationIntegrityWarning]
+_public_interface=[ASKModulator, FSKModulator, ModulationIntegrityWarning]
+
+__all__=[type_obj.__name__ for type_obj in _public_interface]

2845d95386212013d99f2acea5d960ed29d570a0
rlee287 <rlee287@yahoo.com>
2020-01-13 20:01:05 -0800

    Move docstrings in modulator_utils.py to proper place
    (cherry picked from commit 68e45c5403bb25f176c1eda632d92243a376a8fa)

--- voicechat_modem_dsp/modulators/modulator_utils.py
+++ voicechat_modem_dsp/modulators/modulator_utils.py
@@ -2,49 +2,48 @@ import numpy as np
 from scipy import signal
 from scipy.interpolate import interp1d
 
-"""
-Warning to be raised when modulators cannot guarantee data accuracy
-"""
 class ModulationIntegrityWarning(UserWarning):
+    """
+    Warning to be raised when modulators cannot guarantee data accuracy
+    """
     pass
 
-"""
-Computes a time array given a sampling rate and a sample count
-"""
 def generate_timearray(fs, sample_count):
+    """
+    Computes a time array given a sampling rate and a sample count
+    """
     dt=1/fs
     return np.linspace(0,dt*sample_count,sample_count,endpoint=False)
 
-"""
-Computes the number of samples per symbol given a baud and sampling rate
-
-Note: Neither quantity has to be an integer
-"""
 def samples_per_symbol(fs, baud):
+    """
+    Computes the number of samples per symbol given a baud and sampling rate
+
+    Note: Neither quantity has to be an integer
+    """
     return fs/baud
 
-"""
-Maps a sequence (assumed to be sampled at baud rate) to a timesequence
-"""
 def previous_resample_interpolate(timeseq, baud, data):
+    """
+    Maps a sequence (assumed to be sampled at baud rate) to a timesequence
+    """
     interp_func=interp1d(range(len(data)),data,
         kind="previous",copy=False,bounds_error=False,fill_value=0.0)
     return interp_func(timeseq*baud)
 
-"""
-Computes the average of the data over the specified interval with integrals.
-
-The bounds on the given interval need not be integers.
-Linear interpolation is used for noninteger bounds.
-
-Note: Due to trapezoidal approximation this will not produce the
-normal average if the bounds are integers.
-Both endpoints are explicitly included, unlike normal array slicing.
-In addition, the values at the extremities receive half the weight
-as the rest of the data, following the trapezoidal integration formula.
-"""
-
 def average_interval_data(data, begin, end):
+    """
+    Computes the average of the data over the specified interval with integrals.
+
+    The bounds on the given interval need not be integers.
+    Linear interpolation is used for noninteger bounds.
+
+    Note: Due to trapezoidal approximation this will not produce the
+    normal average if the bounds are integers.
+    Both endpoints are explicitly included, unlike normal array slicing.
+    In addition, the values at the extremities receive half the weight
+    as the rest of the data, following the trapezoidal integration formula.
+    """
     if end<begin:
         raise ValueError("End must be larger than begin")
     if begin<0 or end>(len(data)-1):
@@ -82,10 +81,10 @@ def average_interval_data(data, begin, end):
 
     return np.trapz(y_array,x_array)/width
 
-"""
-Computes a gaussian smoothing filter given sampling rate and sigma time
-"""
 def gaussian_window(fs, sigma_dt):
+    """
+    Computes a gaussian smoothing filter given sampling rate and sigma time
+    """
     sigma=sigma_dt*fs
     sample_count=np.ceil(6*sigma+1)
     if sample_count%2==0:
@@ -94,13 +93,13 @@ def gaussian_window(fs, sigma_dt):
     raw_window_sum=np.sum(raw_window)
     return raw_window/raw_window_sum
 
-
-"""
-Uses fred harris' rule-of-thumb to estimate a FIR tap count
-
-Formula from https://dsp.stackexchange.com/questions/37646/filter-order-rule-of-thumb
-"""
 def fred_harris_fir_tap_count(fs, transition_width, db_attenuation):
+    """
+    Uses fred harris' rule-of-thumb to estimate a FIR tap count
+
+    Formula from
+    https://dsp.stackexchange.com/questions/37646/filter-order-rule-of-thumb
+    """
     #N=[fs/delta(f)]∗[atten(dB)/22]
     filter_tap_count=fs/transition_width
     filter_tap_count*=db_attenuation/22
@@ -109,11 +108,11 @@ def fred_harris_fir_tap_count(fs, transition_width, db_attenuation):
         filter_tap_count+=1
     return filter_tap_count
 
-"""
-Computes lowpass FIR filter given cutoffs
-Uses scipy.signal.firls for Least Squares FIR Filter Design
-"""
 def lowpass_fir_filter(fs,cutoff_low,cutoff_high,attenuation=80):
+    """
+    Computes lowpass FIR filter given cutoffs
+    Uses scipy.signal.firls for Least Squares FIR Filter Design
+    """
     if cutoff_low>=cutoff_high:
         raise ValueError("High cutoff must be larger than low cutoff")
     if cutoff_low>=0.5*fs or cutoff_high>=0.5*fs:
@@ -130,23 +129,24 @@ def lowpass_fir_filter(fs,cutoff_low,cutoff_high,attenuation=80):
     # TODO: remove zeros?
     return lowpass_filt
 
-"""
-Helper function that takes symmetric "linear-phase" FIR filter
-and makes it truly linear-phase by removing zeros
-
-Based on the technique described in https://www.cypress.com/file/123191/download
-"""
 def linearize_fir(fir_filter):
+    """
+    Helper function that takes symmetric "linear-phase" FIR filter
+    and makes it truly linear-phase by removing zeros
+
+    Based on the technique described in 
+    https://www.cypress.com/file/123191/download
+    """
     frequencies,response=signal.freqz(fir_filter)
     # TODO fill in the rest of this code if this ends up being actually used
     raise NotImplementedError
 
-"""
-Helper function to computer the IIR filter for the Goertzel algorithm
-
-Derivation of formula from https://www.dsprelated.com/showarticle/796.php
-"""
 def goertzel_iir(freq,fs):
+    """
+    Helper function to computer the IIR filter for the Goertzel algorithm
+
+    Derivation of formula from https://www.dsprelated.com/showarticle/796.php
+    """
     if freq>=0.5*fs:
         raise ValueError("Desired peak frequency is too high")
     norm_freq=2*np.pi*freq/fs

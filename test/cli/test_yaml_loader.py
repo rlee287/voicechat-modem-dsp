d216957f1b50ffa3cb7e7bc68e525117adb21434
rlee287 <rlee287@yahoo.com>
2020-01-18 23:28:27 -0800

    Copy yaml loading tests but use ExtendedCommand static methods this time

--- test/cli/test_yaml_loader.py
+++ test/cli/test_yaml_loader.py
@@ -1,5 +1,6 @@
 from voicechat_modem_dsp.cli.config_loader import parse_config_str, \
     construct_modulators
+from voicechat_modem_dsp.cli.command_utils import CLIError, ExtendedCommand
 from voicechat_modem_dsp.modulators import ASKModulator, FSKModulator
 from .testing_utils import DirectoryChanger, FileCleanup
 
@@ -31,6 +32,26 @@ def test_load_doc_examples():
                 pass
 
 @pytest.mark.unit
+def test_load_doc_examples_staticmethod():
+    with DirectoryChanger("docs/specs/examples"):
+        for yaml_file in glob.iglob("*.yaml"):
+            # YAML is valid <=> no error in parsing
+            config_obj=ExtendedCommand.parse_config_file(yaml_file)
+            modulator_list=construct_modulators(config_obj.data)
+
+            # TODO: remove length check once FDM is in place
+            assert len(modulator_list)==1
+            # TODO: complete for other modes once done
+            if "ask" in yaml_file:
+                assert isinstance(modulator_list[0],ASKModulator)
+            elif "fsk" in yaml_file:
+                assert isinstance(modulator_list[0],FSKModulator)
+            elif "psk" in yaml_file:
+                pass
+            elif "qam" in yaml_file:
+                pass
+
+@pytest.mark.unit
 def test_load_doc_nonexamples():
     with DirectoryChanger("docs/specs/nonexamples"):
         for yaml_file in glob.iglob("*.yaml"):
@@ -40,3 +61,12 @@ def test_load_doc_nonexamples():
             # See TODO notice as to why not check error message contents
             with pytest.raises(YAMLValidationError):
                 config_obj=parse_config_str(config_text)
+
+@pytest.mark.unit
+def test_load_doc_nonexamples_staticmethod():
+    with DirectoryChanger("docs/specs/nonexamples"):
+        for yaml_file in glob.iglob("*.yaml"):
+            # Just make sure this causes an error
+            # See TODO notice as to why not check error message contents
+            with pytest.raises(CLIError):
+                config_obj=ExtendedCommand.parse_config_file(yaml_file)

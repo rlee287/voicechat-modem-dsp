f5a1aeeb8b8bb5fa73155687f9ff0e4f6bb3a8d5
rlee287 <rlee287@yahoo.com>
2020-05-16 19:43:19 -0700

    Adjust docstrings for base modulator object

--- voicechat_modem_dsp/modulators/modulator_base.py
+++ voicechat_modem_dsp/modulators/modulator_base.py
@@ -4,11 +4,10 @@ from abc import ABC, abstractmethod
 
 from typing import Sequence
 
-"""
-Base class for other modulator objects
-Also contains useful helpers as static attributes
-"""
 class BaseModulator(ABC):
+    """
+    Base class with helper functions for other modulator objects
+    """
     # norm.isf(1/(2*2^8))
     sigma_mult_t=2.89
     # norm.isf(0.0001)
@@ -17,7 +16,9 @@ class BaseModulator(ABC):
 
     @abstractmethod
     def modulate(self, datastream: Sequence[int]) -> ndarray:
+        """Modulates the datastream into a signal for transmission"""
         raise NotImplementedError
     @abstractmethod
     def demodulate(self, modulated_data: ndarray) -> Sequence[int]:
+        """Demodulates a signal into a datastream"""
         raise NotImplementedError

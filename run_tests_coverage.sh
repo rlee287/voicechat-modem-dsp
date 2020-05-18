fb686132079c7591899c82612728d392f22d6437
rlee287 <rlee287@yahoo.com>
2020-05-17 19:47:50 -0700

    Parallelize property tests with pytest-cov and pytest-xdist

--- run_tests_coverage.sh
+++ run_tests_coverage.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-COVERAGE_FILE='.coverage.unit' coverage run --branch --source 'test,voicechat_modem_dsp' -m pytest -v -m unit
+# There may be warnings about "no data collected", but this seems to work correctly
+# Only parallelize long property tests for now
+# A cmd unit test needs refactoring to resolve filesystem races first
+pytest --cov=voicechat_modem_dsp --cov=test --cov-report= --cov-branch -v -m unit
 test_status_unit=$?
-COVERAGE_FILE='.coverage.property' coverage run --branch --source 'test,voicechat_modem_dsp' -m pytest -v -m property
+pytest --cov=voicechat_modem_dsp --cov=test --cov-report= --cov-branch --cov-append -n auto -v -m property
 test_status_property=$?
-coverage combine
-coverage report -m
+coverage report
 coverage xml -i
 if [ $test_status_unit -eq 0 ] && [ $test_status_property -eq 0 ]; then
     exit 0
